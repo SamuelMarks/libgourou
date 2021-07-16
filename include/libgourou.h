20b4e69c3e6234bd422da9ec3dc7ee313769872e
Grégory Soutadé <soutade@gmail.com>
2021-07-16 20:44:40 +0200

    Rename too generic "activate" util in "adept_activate"

--- include/libgourou.h
+++ include/libgourou.h
@@ -40,7 +40,7 @@
 #define ACS_SERVER              "http://adeactivate.adobe.com/adept"
 #endif
 
-#define LIBGOUROU_VERSION       "0.2.2"
+#define LIBGOUROU_VERSION       "0.3"
 
 namespace gourou
 {
